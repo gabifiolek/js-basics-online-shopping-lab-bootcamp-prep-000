32cb2e735c6e13bbca703d1bf53e4ca74bcf9e75
Gabriela Fijalkowska <gabrielafijalkowska@gmail.com>
2017-11-12 21:31:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,5 +78,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
+  if (!cardNumber) {
+    console.log ("Sorry, we don't have a credit card on file for you.");
+  } else {
+    console.log (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
+  }
+  return cart;
 }
