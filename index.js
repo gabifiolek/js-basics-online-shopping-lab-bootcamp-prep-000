40bdbc58ae9cd368ddf14025b92b4252f9699f8b
Gabriela Fijalkowska <gabrielafijalkowska@gmail.com>
2017-11-12 21:22:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,10 +71,10 @@ function removeFromCart(item) {
       cart.splice([i], 1);
       return cart;
     }
+  }
     console.log ("That item is not in your cart.");
     return cart;
   }
-}
 
 function placeOrder(cardNumber) {
   // write your code here
